b5f1741720d8892a0f0b7e29df4d8394129fe67f
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2016-03-22 18:59:25 +0100

    Add texture illumination view

---
+++ XwaOptEditor/XwaOptEditor/Converters/IsBpp8Converter.cs
@@ -0,0 +1,31 @@
+﻿using JeremyAnsel.Xwa.Opt;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace XwaOptEditor.Converters
+{
+    class IsBpp8Converter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var texture = value as Texture;
+
+            if (texture == null)
+            {
+                return false;
+            }
+
+            return texture.BitsPerPixel == 8;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XwaOptEditor/XwaOptEditor/Converters/PalletizedTextureConverter.cs
@@ -0,0 +1,39 @@
+﻿using JeremyAnsel.Xwa.Opt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using XwaOptEditor.Helpers;
+
+namespace XwaOptEditor.Converters
+{
+    class PalletizedTextureConverter : BaseConverter, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.Length < 2 ||
+                values[0] == DependencyProperty.UnsetValue ||
+                values[1] == null)
+                return null;
+
+            string pal = (string)values[1];
+
+            if (pal == "Default")
+            {
+                return TextureHelpers.BuildOptTexture(values[0] as Texture, TextureHelpers.DefaultPalette);
+            }
+            else
+            {
+                return TextureHelpers.BuildOptTexture(values[0] as Texture, int.Parse(pal));
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
