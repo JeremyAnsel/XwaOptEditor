8119aa53ce19e46aa89c0f63e002b9467a37e06f
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2019-09-27 20:42:36 +0200

    Add HooksConfig

---
+++ XwaOptEditor/JeremyAnsel.Xwa.HooksConfig/XwaIniSection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JeremyAnsel.Xwa.HooksConfig
+{
+    public sealed class XwaIniSection
+    {
+        public string TxtKey { get; set; }
+
+        public ICollection<string> Lines { get; } = new List<string>();
+
+        public ICollection<string> Settings { get; } = new SortedSet<string>();
+    }
+}
