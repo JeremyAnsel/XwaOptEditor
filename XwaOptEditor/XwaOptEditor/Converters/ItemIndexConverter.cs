e3ab2030c27a256024973225ed274646660c31c3
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2020-04-29 15:43:38 +0200

    Add meshes index

---
+++ XwaOptEditor/XwaOptEditor/Converters/ItemIndexConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace XwaOptEditor.Converters
+{
+    class ItemIndexConverter : FrameworkContentElement, IValueConverter
+    {
+        public ItemIndexConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = DataContext as IList;
+
+            if (list == null)
+            {
+                return string.Empty;
+            }
+
+            int index = list.IndexOf(value);
+
+            return index.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
