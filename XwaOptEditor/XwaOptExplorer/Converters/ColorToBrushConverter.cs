cffce61e6dbfda2a3984b97418ad17492edce31d
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2020-06-09 18:30:21 +0200

    Add render background color

---
+++ XwaOptEditor/XwaOptExplorer/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace XwaOptExplorer.Converters
+{
+    class ColorToBrushConverter : BaseConverter, IValueConverter
+    {
+        public ColorToBrushConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color? color;
+
+            if (value is Color?)
+            {
+                color = (Color?)value;
+            }
+            else if (value is Color)
+            {
+                color = (Color)value;
+            }
+            else
+            {
+                color = null;
+            }
+
+            if (!color.HasValue)
+            {
+                return null;
+            }
+
+            return new SolidColorBrush(color.Value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
